469f4d61eeea658ad065c9587f751817a6cbeb72
Ijas Hussain <ijashuzain@Ijass-MacBook-Pro.local>
2023-03-22 23:10:33 +0530

    mongo connection updated

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config()
 
-connectDb()
-
 const app = express();
   
 const port = process.env.PORT || 5000
@@ -14,6 +12,8 @@ app.use("/api/contacts",require('./routes/contactRoutes'))
 app.use("/api/users",require('./routes/userRoutes'))
 app.use(errorHandler)
 
-app.listen(port,()=>{
-    console.log('server running on port '+port)
+connectDb().then(() => {
+    app.listen(port,()=>{
+        console.log('server running on port '+port)
+    })
 })
