48b2c04c244c79755d0a081d3830e13e3ab2d5d1
Ijas Hussain <ijashuzain@Ijass-MacBook-Pro.local>
2023-03-22 23:18:20 +0530

    db string updated

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
+        const connect = await mongoose.connect("mongodb+srv://administrator:Sledger300@contactscluster.377lu4z.mongodb.net/contactsdb?retryWrites=true&w=majority")
         console.log("Database connected : ",connect.connection.host, connect.connection.name)
     } catch (error) {
         console.log(error);
